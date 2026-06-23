15c640b1fd1eeae78269e658ce42f60662801ff7
falkgeist <43405953+falkgeist@users.noreply.github.com>
2026-06-23 13:58:59 +0200

    Fix typo
    Co-authored-by: cubic-dev-ai[bot] <191113872+cubic-dev-ai[bot]@users.noreply.github.com>

--- contao/languages/de/tl_color.php
+++ contao/languages/de/tl_color.php
@@ -7,7 +7,7 @@ $GLOBALS['TL_LANG']['tl_color']['values_legend'] = 'Werte';
 $GLOBALS['TL_LANG']['tl_color']['title'] = ['Benennung', 'Name des gewählten Farbtons'];
 $GLOBALS['TL_LANG']['tl_color']['variable'] = ['Variablenname', 'Name für die SCSS-Variable (nur Kleinbuchstaben a-z)'];
 $GLOBALS['TL_LANG']['tl_color']['value'] = ['Farbwert', 'Farbe auswählen oder eingeben (hsl[a], rgb[a], hex oder HTML-Name)'];
-$GLOBALS['TL_LANG']['tl_color']['contrast'] = ['Farbeschema | Kontrast', 'Farbe auswählen oder eingeben (hsl[a], rgb[a], hex oder HTML-Name)'];
+$GLOBALS['TL_LANG']['tl_color']['contrast'] = ['Farbschema | Kontrast', 'Farbe auswählen oder eingeben (hsl[a], rgb[a], hex oder HTML-Name)'];
 $GLOBALS['TL_LANG']['tl_color']['isApplicable'] = ['Ist im CMS anwendbar'];
 $GLOBALS['TL_LANG']['tl_color']['category'] = [
     "Für folgende Kategorien verfügbar",
