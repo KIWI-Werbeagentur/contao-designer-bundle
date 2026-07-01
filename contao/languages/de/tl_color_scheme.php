adeaf54b1e3a9a598fbfd391febacfaae9430dd9
Kate <kzimmermann@kiwi.de>
2026-07-01 15:32:16 +0200

    fix: wrong translation declaration

--- contao/languages/de/tl_color_scheme.php
+++ contao/languages/de/tl_color_scheme.php
@@ -1,5 +1,5 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_color_scheme']['colors_legend'] = ['Farben'];
+$GLOBALS['TL_LANG']['tl_color_scheme']['colors_legend'] = 'Farben';
 $GLOBALS['TL_LANG']['tl_color_scheme']['title'] = ['Schemaname'];
 $GLOBALS['TL_LANG']['tl_color_scheme']['alias'] = ['Alias'];
